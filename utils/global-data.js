08c7726cf882f107a60bb1554abde8b420f1d0b1
brittneypostma <bdesignwebdev@gmail.com>
2022-08-25 16:38:48 -0500

    need to fix header

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,17 +1,17 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : "ABC's of Netlify"
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : "ABC's of Netlify"
+  // const footerText = process.env.BLOG_FOOTER_TEXT
+  //   ? decodeURI(process.env.BLOG_FOOTER_TEXT)
+  //   : ""
 
   return {
     name,
     blogTitle,
-    footerText,
-  };
-};
+    // footerText,
+  }
+}

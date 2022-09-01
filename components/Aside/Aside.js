41846239c12d293a15fcc323186e0a802805c052
hexagoncircle <ryan.mulligan@netlify.com>
2022-09-01 14:15:15 -0700

    Update layout responsiveness

--- components/Aside/Aside.js
+++ components/Aside/Aside.js
@@ -4,21 +4,19 @@ import styles from './Aside.module.css';
 export default function Header({ heading }) {
   return (
     <aside className={styles.aside}>
-      <div className={styles.asideContainer}>
-        <Link href="/">
-          {heading ? (
-            <a>
-              <h1 className={styles.title}>
-                ABCs <br /> of Netlify
-              </h1>
-            </a>
-          ) : (
-            <a className={styles.title}>
+      <Link href="/">
+        {heading ? (
+          <a>
+            <h1 className={styles.title}>
               ABCs <br /> of Netlify
-            </a>
-          )}
-        </Link>
-      </div>
+            </h1>
+          </a>
+        ) : (
+          <a className={styles.title}>
+            ABCs <br /> of Netlify
+          </a>
+        )}
+      </Link>
     </aside>
   );
 }

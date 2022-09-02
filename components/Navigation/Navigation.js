e96ecc8342e3435b45934d5266f112770132d4e6
hexagoncircle <ryan.mulligan@netlify.com>
2022-09-02 09:00:33 -0700

    Refactor main layout to grid

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,12 +1,11 @@
 import styles from './Navigation.module.scss';
 
 export default function Navigation({ items }) {
-  console.log(items);
   return (
     <nav className={styles.nav}>
       <ul className={styles.menu}>
         {items.map((item) => (
-          <li key={item.data.title}>
+          <li className={styles.item} key={item.data.title}>
             <a href={`#${item.data.title}`} className={styles.link}>
               {item.data.title}
             </a>

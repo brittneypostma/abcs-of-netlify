b8e7c50be61d6bcf04de4356ddf2071530af8e7a
George Francis <georgedoescode@gmail.com>
2022-09-02 17:07:35 +0100

    Add texture worklet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import dynamic from 'next/dynamic'
+
 
 function MyApp({ Component, pageProps }) {
+  if(typeof window !== 'undefined') {
+    (async function () {
+      if (CSS["paintWorklet"] === undefined) {
+        await import("css-paint-polyfill");
+      }
+      CSS.paintWorklet.addModule('/photocopy-worklet.js');
+    })();
+  }
+
   return (
     <>
       <span className="theme-bejamas" />

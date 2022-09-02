62b4c40dc168cc5db40583d8c2df4423922855a8
hexagoncircle <ryan.mulligan@netlify.com>
2022-09-02 14:55:29 -0700

    Add Netlify logo to Navigation component

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import cn from 'classnames';
 import styles from './Navigation.module.scss';
 
 export default function Navigation({ items, hrefType }) {
   return (
     <nav className={styles.nav}>
       <ul className={styles.menu}>
+        <li className={styles.item}>
+          <Link href="/">
+            <a className={cn(styles.link, styles.logo)}>
+              <Image src="/netlify-logo.svg" alt="" width="30" height="30" />
+            </a>
+          </Link>
+        </li>
         {items.map((item) => (
           <li className={styles.item} key={item.data.title}>
             <a

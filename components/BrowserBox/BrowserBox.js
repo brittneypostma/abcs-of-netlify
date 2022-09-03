59ba30e4c2b07e652cb5fad93bf95d282cd5cfec
hexagoncircle <ryan.mulligan@netlify.com>
2022-09-03 07:41:35 -0700

    Fix BrowserBox video styles

--- components/BrowserBox/BrowserBox.js
+++ components/BrowserBox/BrowserBox.js
@@ -1,5 +1,9 @@
 import styles from './BrowserBox.module.scss';
 
 export default function BrowserBox({ children }) {
-  return <div className={styles.browser}>{children}</div>;
+  return (
+    <div className={styles.browser}>
+      <div className={styles.inner}>{children}</div>
+    </div>
+  );
 }

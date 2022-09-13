2e39296b0b5ae0b9b20753be7a99bad0c25fee0c
brittneypostma <bdesignwebdev@gmail.com>
2022-09-13 12:42:37 -1000

    videos added

--- components/Video.js
+++ components/Video.js
@@ -1,11 +1,23 @@
-import BrowserBox from './BrowserBox/BrowserBox';
+import BrowserBox from './BrowserBox/BrowserBox'
 
-export default function Video({ id }) {
-  const videoId = id || 'COAVmST41Q0';
+export default function Video({ id, video }) {
+  const videoId = id || 'COAVmST41Q0'
 
   return (
     <BrowserBox>
-      <iframe
+      <video
+        controls
+        width="560"
+        height="315"
+        autoPlay={false}
+        preload="metadata"
+        playsInline
+      >
+        <source
+          src={`/video/letters/${video}.mp4`} type="video/mp4" />
+        Sorry, your browser doesn’t support embedded videos.
+      </video>
+      {/* <iframe
         width="560"
         height="315"
         src={`https://www.youtube.com/embed/${videoId}`}
@@ -13,7 +25,7 @@ export default function Video({ id }) {
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-      ></iframe>
+      ></iframe> */}
     </BrowserBox>
-  );
+  )
 }
